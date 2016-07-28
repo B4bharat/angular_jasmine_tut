9f592f7e5807fa705495bd8f9d4a1f1addf583b9
Bharat Poptwani <bharat@sixmod5.com>
2016-07-28 19:14:34 +0530

    added tests for checking if all the users exist

--- app/services/users/users.js
+++ app/services/users/users.js
@@ -5,6 +5,41 @@
   .factory('Users', function() {
     var Users = {};
 
+    var userList = [
+    {
+      id: '1',
+      name: 'Jane',
+      role: 'Designer',
+      location: 'New York',
+      twitter: 'gijane'
+    },
+    {
+      id: '2',
+      name: 'Bob',
+      role: 'Developer',
+      location: 'New York',
+      twitter: 'billybob'
+    },
+    {
+      id: '3',
+      name: 'Jim',
+      role: 'Developer',
+      location: 'Chicago',
+      twitter: 'jimbo'
+    },
+    {
+      id: '4',
+      name: 'Bill',
+      role: 'Designer',
+      location: 'LA',
+      twitter: 'dabill'
+    }
+  ];
+
+    Users.all = function() {
+      return userList;
+    };
+
     return Users;
   })
 })();
--- app/services/users/users.spec.js
+++ app/services/users/users.spec.js
@@ -1,6 +1,37 @@
 describe('Users factory', function() {
   var Users;
 
+  var userList = [
+    {
+      id: '1',
+      name: 'Jane',
+      role: 'Designer',
+      location: 'New York',
+      twitter: 'gijane'
+    },
+    {
+      id: '2',
+      name: 'Bob',
+      role: 'Developer',
+      location: 'New York',
+      twitter: 'billybob'
+    },
+    {
+      id: '3',
+      name: 'Jim',
+      role: 'Developer',
+      location: 'Chicago',
+      twitter: 'jimbo'
+    },
+    {
+      id: '4',
+      name: 'Bill',
+      role: 'Designer',
+      location: 'LA',
+      twitter: 'dabill'
+    }
+  ];
+
   //Before each test load our api.users module
   beforeEach(angular.mock.module('api.users'));
 
@@ -12,5 +43,18 @@ describe('Users factory', function() {
   // A simple test to verify the Users factory exists
   it('should exist', function() {
     expect(Users).toBeDefined();
+  });
+
+  // A set of tests for our Users.all() method
+  describe('.all()', function() {
+    // A simple test to verify the method all exists
+    it('should exist', function() {
+      expect(Users.all).toBeDefined();
+    });
+
+    // A test to verify that calling all() returns the array of users we hard-coded above
+    it('should return a hard-coded list of users', function() {
+      expect(Users.all()).toEqual(userList);
+    });
   });
 });
